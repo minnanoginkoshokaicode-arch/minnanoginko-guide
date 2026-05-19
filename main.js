bf5357bb17ae2d2d6ecd8d02703ec77cc8bab914
Yu Omura <yuomura@YunoMacBook-Pro.local>
2026-05-19 14:18:16 +0900

    タイトル修正

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var _m = d.getMonth() + 1;
 var _day = d.getDate();
 var _text = _y + '年' + _m + '月' + _day + '日最新';
 document.getElementById('dateBadge').textContent = _text;
-document.title = '【最短翌日】みんなの銀行紹介コードで1000円！' + _text + 'の口座開設特典ガイド';
+document.title = '【最短翌日】みんなの銀行 紹介コード・初回特典コードで1000円！' + _y + '年' + _m + '月最新ガイド';
 
 function copyText(btnId) {
   var text = 'WdMQHEHZ';
